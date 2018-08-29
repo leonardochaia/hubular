f107b6c2b6706b89e88563ea18043b29d5f09845
Leonardo Chaia <lchaia@astonishinglab.com>
2018-08-29 01:27:07 -0300

    chore: fixed decorators not included in public api

--- lib/index.ts
+++ lib/index.ts
@@ -6,3 +6,6 @@ export * from './src/hubular-robot.model';
 
 export { bootstrapModule } from './src/bootstrap';
 export { HubularModule } from './src/hubular-module.decorator';
+
+export { RobotHear } from './src/robot-hear.decorator';
+export { RobotRespond } from './src/robot-respond.decorator';
