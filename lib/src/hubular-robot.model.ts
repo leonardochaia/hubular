6d6db04a79d65cbd67d218db9886c647cd735327
Leonardo Chaia <lchaia@astonishinglab.com>
2018-09-16 10:45:12 -0300

    feat: improvements on brain typing

--- lib/src/hubular-robot.model.ts
+++ lib/src/hubular-robot.model.ts
@@ -1,5 +1,6 @@
 import { Robot, Brain } from 'hubot';
 import { Injector } from 'injection-js';
+import { EventEmitter } from 'events';
 
 export interface HubularRobot<TAdapter = any> extends Robot<TAdapter> {
     injector: Injector;
@@ -12,7 +13,9 @@ export interface HubularRobot<TAdapter = any> extends Robot<TAdapter> {
     };
 }
 
-export interface HubularRobotBrain extends Brain {
+export interface HubularRobotBrain extends Brain, EventEmitter {
     get<T>(key: string): T;
     set<T>(key: string, value: T): void;
+    setAutoSave(status: boolean): void;
+    mergeData(data: any): void;
 }
